193b4be0d2cdf56ea7c1a42073fac6b077a7923a
Daniel Cave <49564558+danieljcave@users.noreply.github.com>
2023-07-10 20:48:29 +0100

    add questions to js and add current to quiz box

---
+++ assets/js/questions.js
@@ -0,0 +1,60 @@
+let questions = [
+    {
+        num: 1,
+        question: 'When squatting which muscle is not used?',
+        answer: 'D. Biceps',
+        options: [
+            'A. Glutes',
+            'B. Quads',
+            'C. Hamstrings',
+            'D. Biceps',
+        ]
+    },
+    {
+        num: 2,
+        question: '2. Which exercise targets the lats when working out?',
+        answer: 'D. Cable Pull Down',
+        options: [
+            'A. Hyperextensions',
+            'B. Overhead Press',
+            'C. Box Squats',
+            'D. Cable Pull Down',
+        ]
+    },
+    {
+        num: 3,
+        question: '3. If you run 5KM run in 26:05, what is you 1km pace speed?',
+        answer: 'A. 5 Minutes 13 Seconds',
+        options: [
+            'A. 5 Minutes 13 Seconds',
+            'B. 5 minutes 45 seconds',
+            'C. 4 minutes 50 seconds',
+            'D. 5 minutes 22 seconds',
+            
+        ]
+    },
+    {
+        num: 4,
+        question: '4. What muscle is most targeted when doing an overhead press movement?',
+        answer: 'D. Shoulders',
+        options: [
+            'A. Biceps',
+            'B. Chest',
+            'C. Back',
+            'D. Shoulders',
+            
+        ]
+    },
+    {
+        num: 5,
+        question: '5. In fitness, what does AMRAP stand for?',
+        answer: 'C. As Many Reps As Possible',
+        options: [
+            'A. All My Rest and Prep',
+            'B. Any Means Rest and Recovery',
+            'C. As Many Reps As Possible',
+            'D. As Many Rounds and Pushes',
+        
+        ]
+    },
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ const quizExit = document.querySelector('.exit-btn')
 const quizMain = document.querySelector('.main')
 const continueBtn = document.querySelector('.continue-btn')
 const quizSection = document.querySelector('.quiz-section')
+const quizBox = document.querySelector('.quiz-box')
 
 quizStart.onclick = () => {
     quizPopup.classList.add('current');
@@ -19,4 +20,5 @@ continueBtn.onclick = () => {
     quizSection.classList.add('current');
     quizPopup.classList.remove('current');
     quizMain.classList.remove('current');
+    quizBox.classList.add('current');
 }
