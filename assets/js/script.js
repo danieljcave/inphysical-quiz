44c24a54ca4d03d7c6cd1d5a930dd32f21e97f58
Daniel Cave <49564558+danieljcave@users.noreply.github.com>
2023-07-10 12:41:09 +0100

    add background blue to page when popup is shown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 const quizStart = document.querySelector('.quiz-btn')
 const quizPopup = document.querySelector('.start-quiz-popup')
 const quizExit = document.querySelector('.exit-btn')
+const quizMain = document.querySelector('.main')
 
 quizStart.onclick = () => {
     quizPopup.classList.add('current');
+    quizMain.classList.add('current');
 }
 
 quizExit.onclick = () => {
     quizPopup.classList.remove('current');
+    quizMain.classList.remove('current');
 }
