30a8311db22455c819983188a1e4823b088165a1
Daniel Cave <49564558+danieljcave@users.noreply.github.com>
2023-07-10 12:14:16 +0100

    add js to quiz popup to display info and start quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-console.log('Test!')
+const quizStart = document.querySelector('.quiz-btn')
+const quizPopup = document.querySelector('.start-quiz-popup')
+
+quizStart.onclick = () => {
+    quizPopup.classList.add('current');
+}
