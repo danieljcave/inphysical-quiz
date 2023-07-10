b8a5316fb60600f1c26f1acfb360aacaaeee3968
Daniel Cave <49564558+danieljcave@users.noreply.github.com>
2023-07-10 12:19:10 +0100

    add js to remove current selector from code to remove popup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 const quizStart = document.querySelector('.quiz-btn')
 const quizPopup = document.querySelector('.start-quiz-popup')
+const quizExit = document.querySelector('.exit-btn')
 
 quizStart.onclick = () => {
     quizPopup.classList.add('current');
+}
+
+quizExit.onclick = () => {
+    quizPopup.classList.remove('current');
 }
