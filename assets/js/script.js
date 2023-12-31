f5af35aa4b75fa69dc56eb5109d1b9899ee006f5
Daniel Cave <49564558+danieljcave@users.noreply.github.com>
2023-12-31 13:55:16 +0000

    update script file with array and ad jshint for testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const continueBtn = document.querySelector('.continue-btn');
+/*jshint esversion: 6 */
 const quizSection = document.querySelector('.quiz-section');
 const quizBox = document.querySelector('.quiz-box');
 const resultBox = document.querySelector('.result-box');
@@ -121,3 +121,63 @@ function showResultBox() {
     const scoreText = document.querySelector('.score-text');
     scoreText.textContent = `You Scored ${userScore} Out Of ${questions.length}`;
 }
+
+let questions = [{
+    numb: 1,
+    question: 'When squatting which muscle is not used?',
+    answer: 'D. Biceps',
+    options: [
+        'A. Glutes',
+        'B. Quads',
+        'C. Hamstrings',
+        'D. Biceps',
+    ]
+},
+{
+    numb: 2,
+    question: 'Which exercise targets the lats when working out?',
+    answer: 'D. Cable Pull Down',
+    options: [
+        'A. Hyperextensions',
+        'B. Overhead Press',
+        'C. Box Squats',
+        'D. Cable Pull Down',
+    ]
+},
+{
+    numb: 3,
+    question: 'If you run 5KM run in 26:05, what is you 1km pace speed?',
+    answer: 'A. 5 Minutes 13 Seconds',
+    options: [
+        'A. 5 Minutes 13 Seconds',
+        'B. 5 minutes 45 seconds',
+        'C. 4 minutes 50 seconds',
+        'D. 5 minutes 22 seconds',
+
+    ]
+},
+{
+    numb: 4,
+    question: 'What muscle is most targeted when doing an overhead press movement?',
+    answer: 'D. Shoulders',
+    options: [
+        'A. Biceps',
+        'B. Chest',
+        'C. Back',
+        'D. Shoulders',
+
+    ]
+},
+{
+    numb: 5,
+    question: 'In fitness, what does AMRAP stand for?',
+    answer: 'C. As Many Reps As Possible',
+    options: [
+        'A. All My Rest and Prep',
+        'B. Any Means Rest and Recovery',
+        'C. As Many Reps As Possible',
+        'D. As Many Rounds and Pushes',
+
+    ]
+},
+];
